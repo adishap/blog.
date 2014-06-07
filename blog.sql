e2b04bcedba0c8aa9eb3532c135275c7295b6a48
MY_EMAIL <porwaladisha@gmail.com>
2014-06-07 12:59:02 +0530

    chnage in .sql file

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 05, 2014 at 08:01 AM
+-- Generation Time: Jun 07, 2014 at 09:05 AM
 -- Server version: 5.5.36
 -- PHP Version: 5.4.25
 
@@ -49,8 +49,21 @@ CREATE TABLE IF NOT EXISTS `article` (
   `user_id` varchar(8) NOT NULL,
   `description` varchar(100) NOT NULL,
   `cat_id` varchar(2) NOT NULL,
-  PRIMARY KEY (`art_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `status` varchar(11) NOT NULL,
+  `title` varchar(15) NOT NULL,
+  PRIMARY KEY (`art_id`),
+  UNIQUE KEY `art_id` (`art_id`),
+  UNIQUE KEY `art_id_2` (`art_id`),
+  UNIQUE KEY `art_id_3` (`art_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `article`
+--
+
+INSERT INTO `article` (`art_id`, `user_id`, `description`, `cat_id`, `status`, `title`) VALUES
+(1, 'adisha', 'dsgsjj', '1', 'approved', 'adfdgh'),
+(2, 'adisha', 'ghzj', '2', 'approved', 'xcfhng');
 
 -- --------------------------------------------------------
 
@@ -59,9 +72,22 @@ CREATE TABLE IF NOT EXISTS `article` (
 --
 
 CREATE TABLE IF NOT EXISTS `catogry` (
-  `cat_id` int(2) NOT NULL,
-  `cat` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `cat_id` int(2) NOT NULL AUTO_INCREMENT,
+  `cat` varchar(15) NOT NULL,
+  UNIQUE KEY `cat_id` (`cat_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `catogry`
+--
+
+INSERT INTO `catogry` (`cat_id`, `cat`) VALUES
+(1, 'music'),
+(2, 'technical'),
+(3, 'dance'),
+(4, 'management'),
+(5, 'cooking'),
+(6, 'food');
 
 -- --------------------------------------------------------
 
@@ -81,7 +107,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`user_id`, `pass`, `fname`, `sname`) VALUES
-('adisha', '32250170a0dca92d53ec9624f336ca24', 'adisha', 'porwal');
+('adisha', '32250170a0dca92d53ec9624f336ca24', 'adisha', 'porwal'),
+('hetald', '32250170a0dca92d53ec9624f336ca24', 'hetal', 'desai');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
